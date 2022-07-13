7b30bb71f9167d34c92e8c057b80b6a4b575b3a2
GuyRo1 <Guyrossert@gmail.com>
2022-07-13 22:38:50 +0300

    fix

--- src/components/Options/Options.tsx
+++ src/components/Options/Options.tsx
@@ -8,16 +8,18 @@ type Props = {
 const Options = ({ update }: Props) => {
 
     return (
-        <div className="options">
-            <div className='option' onClick={()=>{update(1)}}>1</div>
-            <div className='option' onClick={()=>{update(2)}}>2</div>
-            <div className='option' onClick={()=>{update(3)}}>3</div>
-            <div className='option' onClick={()=>{update(4)}}>4</div>
-            <div className='option' onClick={()=>{update(5)}}>5</div>
-            <div className='option' onClick={()=>{update(6)}}>6</div>
-            <div className='option' onClick={()=>{update(7)}}>7</div>
-            <div className='option' onClick={()=>{update(8)}}>8</div>
-            <div className='option' onClick={()=>{update(9)}}>9</div>
+        <div className='container'>
+            <div className="options">
+                <div className='option' onClick={() => { update(1) }}>1</div>
+                <div className='option' onClick={() => { update(2) }}>2</div>
+                <div className='option' onClick={() => { update(3) }}>3</div>
+                <div className='option' onClick={() => { update(4) }}>4</div>
+                <div className='option' onClick={() => { update(5) }}>5</div>
+                <div className='option' onClick={() => { update(6) }}>6</div>
+                <div className='option' onClick={() => { update(7) }}>7</div>
+                <div className='option' onClick={() => { update(8) }}>8</div>
+                <div className='option' onClick={() => { update(9) }}>9</div>
+            </div>
         </div>
     )
 }
--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,4 +1,4 @@
-import {useEffect} from 'react';
+
 import './Question.css'
 
 type Props = { question: number[] }
@@ -8,12 +8,14 @@ const Question = ({ question }: Props) => {
 
 
     return (
-        <div className="question">
-            <div className="section">{question[0] ? question[0] : '?'}</div>
-            <div className="section">{question[1] ? question[1] : '?'}</div>
-            <div className="section">{question[2] ? question[2] : '?'}</div>
-            <div className="section">{question[3] ? question[3] : '?'}</div>
-        </div>
+     
+            <div className="question">
+                <div className="section">{question[0] ? question[0] : '?'}</div>
+                <div className="section">{question[1] ? question[1] : '?'}</div>
+                <div className="section">{question[2] ? question[2] : '?'}</div>
+                <div className="section">{question[3] ? question[3] : '?'}</div>
+            </div>
+    
 
     )
 }
